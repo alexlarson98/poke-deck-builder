3c82410f8b9647835bc7d0d128ce823f1cbaa5e5
Alex Larson <alexlarson98@yahoo.com>
2023-03-05 12:38:47 -0500

    add 404, logo, beautify article pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ArticleComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
